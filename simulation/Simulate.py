accc37cd55d0a5f049fbf7af9accbcfad23d2067
atenagm <ashenatena@gmail.com>
2020-04-12 17:13:04 +0430

    implemented lif, elif, adaptive_lif, and simulation and monitor stuff. warning: not tested and debugged

--- simulation/Simulate.py
+++ simulation/Simulate.py
@@ -8,17 +8,14 @@ class Simulate:
         self.neuron = neuron
 
     def run(self, time_window, current):
-        spike_times = []
         potential_list = []
         current_list = []
         time_list = []
         time_interval = range(self.__t, self.__t + time_window, self.dt)
         for t in time_interval:
-            u, spiked = neuron.simulate(current, t, self.dt)
+            u = neuron.simulate(current, t, self.dt)
             potential_list.append(u)
             current_list.append(current(t))
             time_list.append(t)
             self.__t = t
-            if spiked:
-                spike_times.append(t)
-        return potential_list, current_list, spike_times, time_list
+        return potential_list, current_list, time_list
