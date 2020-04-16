561e451177d544f880530187c761180d5834f770
atenagm <ashenatena@gmail.com>
2020-04-16 09:48:36 +0430

    bug fix in equation

--- models/Neurons.py
+++ models/Neurons.py
@@ -19,7 +19,7 @@ class LIF:
         return self._u
 
     def __new_u(self, current, t, dt):
-        return self._u + self._tau_du_dt(current(t)) * self.tau * dt
+        return self._u + self._tau_du_dt(current(t)) * (1 / self.tau) * dt
 
     def _tau_du_dt(self, i):
         return -(self._u - self.u_rest) + self.r * i
@@ -41,11 +41,11 @@ class ELIF(LIF):
         return super(ELIF, self)._tau_du_dt(i) + self.delta_t * np.exp((self._u - self.threshold) / self.delta_t)
 
     def __new_u(self, current, t, dt):
-        return self._u + self._tau_du_dt(current(t)) * self.tau * dt
+        return self._u + self._tau_du_dt(current(t)) * (1 / self.tau) * dt
 
 
 class AddaptiveELIF(ELIF):
-    def __init__(self, tau=10, u_rest=-70, r=5, threshold=-50, delta_t=1, tau_w=5, w=0, a=1, b=1):
+    def __init__(self, tau=10, u_rest=-70, r=5, threshold=-50, delta_t=1, tau_w=5, w=2, a=5, b=1):
         super(AddaptiveELIF, self).__init__(tau, u_rest, r, threshold, delta_t)
         self.tau_w = tau_w
         self.__w = w
@@ -61,11 +61,11 @@ class AddaptiveELIF(ELIF):
 
     def _simulate(self, current, t, dt):
         self._u = self.__new_u(current, t, dt)
-        self.__w = self.__w + self._tau_dw_dt(t) * self.tau_w * dt
         if self._u >= self.threshold:
             self._u = self.u_rest
             self.spike_times.append(t)
+        self.__w = self.__w + self._tau_dw_dt(t) * (1 / self.tau_w) * dt
         return self._u
 
     def __new_u(self, current, t, dt):
-        return self._u + self._tau_du_dt(current(t)) * self.tau * dt
+        return self._u + self._tau_du_dt(current(t)) * (1 / self.tau) * dt
