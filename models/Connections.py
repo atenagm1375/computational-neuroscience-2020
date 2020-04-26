cc1cf217a90da24aeb4b89f478579fdfaf8e3b38
atenagm <ashenatena@gmail.com>
2020-04-26 15:55:01 +0430

    add synapse and connection

---
+++ models/Connections.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from copy import deepcopy
+
+
+class Connection:
+    def __init__(self, pre, post, synapse, alpha):
+        self.pre = pre
+        self.post = post
+        self.synapse = synapse
+
+        self.pattern = [[]] * self.post.number
+        self.alpha = alpha
+
+    def apply(self, connection_type, **kwargs):
+        if connection_type == "full":
+            self.synapse.w = self.synapse.w / self.post.number
+            for i in range(self.pre.number):
+                for j in range(self.post.number):
+                    self.pattern[j].append([i, deepcopy(self.synapse)])
+
+        elif connection_type == "fixed_prob":
+            w, h = self.pre.number, self.post.number
+            n = int(kwargs["p"] * h)
+            self.synapse.w = self.synapse.w / n
+            points = [divmod(i, h) for i in np.random.choice(range(w * h), n)]
+            for x in points:
+                self.pattern[x[1]].append([x[0], deepcopy(self.synapse)])
+
+        elif connection_type == "fixed_pre":
+            n = int(kwargs["p"] * self.post.number)
+            for j in range(self.post.number):
+                pres = np.random.choice(range(self.pre.number), n)
+                for i in pres:
+                    self.pattern[j].append([i, deepcopy(self.synapse)])
+
+        else:
+            raise ValueError("Invalid connection type!")
+
+    def post_input(self, t):
+        for i in range(self.post.number):
+            for j, syn in self.pattern[i]:
+                self.post[i].input += syn.w * self.pre.effect(self.alpha, t)
---
+++ models/Synapses.py
@@ -0,0 +1,4 @@
+class Synapse:
+    def __init__(self, weight, delay=0):
+        self.w = weight
+        self.d = delay
