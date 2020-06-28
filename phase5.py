f39942d53795f3bbcedfde5ab474a0c705f114e0
atenagm <ashenatena@gmail.com>
2020-06-28 14:21:53 +0430

    add test for question 1

--- phase5.py
+++ phase5.py
@@ -1,15 +1,24 @@
-from PIL import Image
+from tests.phase5.phase5 import *
+import sys
 
+from PIL import Image
 from image_processing.filters import *
 from image_processing.convolution import Convolution
 from simulation.Monitors import plot_images
 
 
 def q1(sigma1, sigma2, filter_sizes, k):
-    image = np.asarray(Image.open('tests/phase5/car.jpg').convert('LA'))
-    kernel = DoG(sigma1, sigma2)
+    image = np.asarray(Image.open('tests/phase5/car.jpg').resize((800, 400)).convert('L'))
     for n in filter_sizes:
+        kernel = DoG(sigma1, sigma2).apply((n, n))
         conv = Convolution(n)
         filtered_im = conv.apply(image, kernel)
 
         plot_images([(image, "Original image"), (filtered_im, "DoG result")])
+
+
+if __name__ == "__main__":
+    q = sys.argv[1]
+    t = sys.argv[2]
+
+    globals()["q{}".format(q)](*globals()["q{}_t{}".format(q, t)]())
--- tests/phase5/phase5.py
+++ tests/phase5/phase5.py
@@ -1,6 +1,6 @@
 def q1_t1():
     sigma1 = 0.3
     sigma2 = 1.0
-    sizes = [3, 5, 7, 9, 11]
+    sizes = [3, 7, 11, 15, 19]
     k = 3
     return sigma1, sigma2, sizes, k
