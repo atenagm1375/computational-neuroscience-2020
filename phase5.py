7acb023fefeaed8e81e92676c474ff3133628528
atenagm <ashenatena@gmail.com>
2020-06-28 15:51:13 +0430

    add test for question 2

--- phase5.py
+++ phase5.py
@@ -9,12 +9,37 @@ from simulation.Monitors import plot_images
 
 def q1(sigma1, sigma2, filter_sizes, k):
     image = np.asarray(Image.open('tests/phase5/car.jpg').resize((800, 400)).convert('L'))
+
     for n in filter_sizes:
         kernel = DoG(sigma1, sigma2).apply((n, n))
         conv = Convolution(n)
         filtered_im = conv.apply(image, kernel)
 
-        plot_images([(image, "Original image"), (filtered_im, "DoG result")])
+        plot_images(np.array([image, filtered_im]), np.array(["Original Image", "Image after DoG filter"]))
+
+
+def q2(lambd, sigma, gamma, n_orientations, filter_sizes):
+    image = np.asarray(Image.open('tests/phase5/car.jpg').resize((800, 400)).convert('L'))
+    # plot_images(np.array([image]), np.array(["Original Image"]))
+
+    filtered_images = []
+    kernels = []
+    titles = []
+    for ind, n in enumerate(filter_sizes):
+        filtered_images.append([])
+        titles.append([])
+        kernels.append([])
+        for i in range(n_orientations):
+            theta = i * np.pi / n_orientations
+            kernel = Gabor(lambd, theta, sigma, gamma).apply((n, n))
+            kernels[-1].append(kernel)
+            conv = Convolution(n)
+            filtered_im = conv.apply(image, kernel)
+            filtered_images[-1].append(filtered_im)
+            titles[-1].append("filter #{}: size={}, theta={}".format(ind, n, theta))
+
+    plot_images(np.array(kernels))
+    plot_images(np.array(filtered_images))
 
 
 if __name__ == "__main__":
--- tests/phase5/phase5.py
+++ tests/phase5/phase5.py
@@ -4,3 +4,12 @@ def q1_t1():
     sizes = [3, 7, 11, 15, 19]
     k = 3
     return sigma1, sigma2, sizes, k
+
+
+def q2_t1():
+    lambd = 0.2
+    sigma = 0.1
+    gamma = 0.01
+    n_orientations = 4
+    filter_sizes = [3, 5, 7, 9, 11, 13, 15, 17]
+    return lambd, sigma, gamma, n_orientations, filter_sizes
