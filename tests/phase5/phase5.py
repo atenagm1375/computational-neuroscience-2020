665e11b234d6d81e7cd339cb577e1892cf116459
atenagm <ashenatena@gmail.com>
2020-06-29 12:03:11 +0430

    add more trials for question 2

--- tests/phase5/phase5.py
+++ tests/phase5/phase5.py
@@ -18,3 +18,75 @@ def q2_t1():
     stride = 1
     time_window = 20
     return lambd, sigma, gamma, n_orientations, filter_sizes, pad, stride, time_window
+
+
+def q2_t2():
+    lambd = 10
+    sigma = 1
+    gamma = 0.5
+    n_orientations = 4
+    filter_sizes = [5, 13, 21, 29, 37, 45, 53, 61]
+    pad = False
+    stride = 1
+    time_window = 20
+    return lambd, sigma, gamma, n_orientations, filter_sizes, pad, stride, time_window
+
+
+def q2_t3():
+    lambd = 5
+    sigma = 1
+    gamma = 0.5
+    n_orientations = 4
+    filter_sizes = [5, 13, 21, 29, 37, 45, 53, 61]
+    pad = False
+    stride = 1
+    time_window = 20
+    return lambd, sigma, gamma, n_orientations, filter_sizes, pad, stride, time_window
+
+
+def q2_t4():
+    lambd = 10
+    sigma = 1
+    gamma = 0.2
+    n_orientations = 4
+    filter_sizes = [5, 13, 21, 29, 37, 45, 53, 61]
+    pad = False
+    stride = 1
+    time_window = 20
+    return lambd, sigma, gamma, n_orientations, filter_sizes, pad, stride, time_window
+
+
+def q2_t5():
+    lambd = 10
+    sigma = 2
+    gamma = 0.2
+    n_orientations = 4
+    filter_sizes = [5, 13, 21, 29, 37, 45, 53, 61]
+    pad = False
+    stride = 1
+    time_window = 20
+    return lambd, sigma, gamma, n_orientations, filter_sizes, pad, stride, time_window
+
+
+def q2_t6():
+    lambd = 10
+    sigma = 2
+    gamma = 0.2
+    n_orientations = 4
+    filter_sizes = [5, 13, 21, 29, 37, 45, 53, 61]
+    pad = True
+    stride = 1
+    time_window = 20
+    return lambd, sigma, gamma, n_orientations, filter_sizes, pad, stride, time_window
+
+
+def q2_t7():
+    lambd = 10
+    sigma = 2
+    gamma = 0.2
+    n_orientations = 4
+    filter_sizes = [5, 13, 21, 29, 37, 45, 53, 61]
+    pad = False
+    stride = 2
+    time_window = 20
+    return lambd, sigma, gamma, n_orientations, filter_sizes, pad, stride, time_window
